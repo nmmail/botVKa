d86955359f0ada076ccdffa4631d9b291ca6507f
ValiakhmetovBulat <valiaxmetovb134@gmail.com>
2021-06-18 20:27:44 +0300

    first words

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard
 from toks import main_token
+import openpyxl
+from openpyxl import load_workbook
+
+wb_form = load_workbook(filename='123.xlsx')
+wb_val = load_workbook(filename='123.xlsx', data_only=True)
+
+sheet_form = wb_form['Лист 1']
+sheet_val = wb_val['Лист 1']
+
+E5_form = wb_val['E5'].value
+
+print(E5_form)
 
 vk_session = vk_api.VkApi(token=main_token)
 session_api = vk_session.get_api()
@@ -16,7 +29,6 @@ for event in longpoll.listen():
             msg = event.text.lower()
             id = event.user_id
 
-            if (msg == 'привет') or (msg == 'начать') or (msg == 'здравствуйте'):
-                sender(id, 'Здравствуй, уважаемый гость!\nОзнакомиться с каталогом можно в нашем Instagram:\nhttps://www.instagram.com/tkanifurnitura_kazan/')
+
 
 
