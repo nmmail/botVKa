7b5859a9a2ea94ecf0bc3cf412c2dcd9f5538a81
ValiakhmetovBulat <Ghost1029>
2021-06-18 20:27:52 +0300

    asd

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from toks import main_token
 vk_session = vk_api.VkApi(token=main_token)
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
-
+ dsfasdfdsafsdfasdf
 def sender(id, text) :
     vk_session.method('messages.send', {'user_id': id, 'message': text, 'random_id': 0})
 
