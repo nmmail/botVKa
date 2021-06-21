b2977c667c8d323f30d8b336ad4cab67b49f3f66
ValiakhmetovBulat <Ghost1029>
2021-06-21 03:38:32 +0300

    вкусный конь через 3 часа экз

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard
 from toks import main_token
+
+
+vk_session = vk_api.VkApi(token=main_token)
+session_api = vk_session.get_api()
+longpoll = VkLongPoll(vk_session)
+
 import openpyxl
 
 my_path = "123.xlsx"
@@ -9,12 +15,47 @@ my_wb_obj = openpyxl.load_workbook(my_path)
 my_sheet_obj = my_wb_obj.active
 string = ''
 
-vk_session = vk_api.VkApi(token=main_token)
-session_api = vk_session.get_api()
-longpoll = VkLongPoll(vk_session)
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+def sender(id, text, keybord=None) :
+    post = {'user_id': id, 'message': text, 'random_id': 0}
+    if keybord != None:
+        post["keyboard"] = keybord.get_keyboard()
+    else:
+        post = post
+    vk_session.method('messages.send', post)
 
-def sender(id, text) :
-    vk_session.method('messages.send', {'user_id': id, 'message': text, 'random_id': 0})
+def readerEx(msg):
+    c = 1
+    p = 1
+    for cell in my_sheet_obj['A']:
+        if str(msg) == str(cell.value):
+            for c in range(2, 9):
+                string = string + str((my_sheet_obj.cell(row=p, column=c)).value) + ' '
+
+            sender(id, string + '')
+        string = ''
+        p = p + 1
+        c = 1
+        print(p)
+        # this reader
+
+
+def button():
+     keyboard = VkKeyboard(one_time=False)
+     buttosn = ["Пондельник", "Вторник", "Среда" , "Четверг" , "Пятница" , "Суббота" ]
+     buttosn_color = [VkKeyboardColor.PRIMARY,VkKeyboardColor.PRIMARY,VkKeyboardColor.PRIMARY,VkKeyboardColor.PRIMARY,VkKeyboardColor.PRIMARY,VkKeyboardColor.PRIMARY]
+
+     trg = 0
+     for btn, btn_c in zip(buttosn, buttosn_color):
+         trg = trg + 1
+         keyboard.add_button(btn, btn_c)
+         if trg == 2:
+             if(btn != 'Суббота'):
+                 keyboard.add_line()
+             trg = 0
+     sender(id, "Выберите день недели :", keyboard)
+
+
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
@@ -28,18 +69,12 @@ for event in longpoll.listen():
                     break
 
             if(flag_f == True):
-                 c = 1
-                 p = 1
-                 for cell in my_sheet_obj['A']:
-                     if str(msg) == str(cell.value):
-                                for c in range(2, 9):
-                                    string = string + str((my_sheet_obj.cell(row=p, column=c)).value) + ' '
+                 from toks import days
+                 button()
+                 if(msg == days[0]):
+                     sender(id, "Расписание на понедельник :")
+                 #readerEx(msg)
 
-                                sender(id, string)
-                     string = ''
-                     p = p + 1
-                     c = 1
-                     print(p)
 
             else :
                 sender(id , 'Группа не найдена.Повторите')
